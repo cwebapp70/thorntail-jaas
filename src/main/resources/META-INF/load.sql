8198c2f99176374de4894a19a15c5c8a81fa36a2
francesco <francesco@fedora.redhat.com>
2019-04-16 12:55:11 +0200

    first attempt

---
+++ src/main/resources/META-INF/load.sql
@@ -0,0 +1,10 @@
+CREATE TABLE REST_DB_ACCESS (ID INT, NAME VARCHAR(50), PASSWORD VARCHAR(50), ROLE VARCHAR(50))
+
+INSERT INTO REST_DB_ACCESS("ID", "NAME", "PASSWORD", "ROLE") VALUES (1, 'Penny', 'password', 'admin')
+INSERT INTO REST_DB_ACCESS("ID", "NAME", "PASSWORD", "ROLE") VALUES (2, 'Sheldon', 'password', 'user')
+INSERT INTO REST_DB_ACCESS("ID", "NAME", "PASSWORD", "ROLE") VALUES (3, 'Amy', 'password', 'guest')
+INSERT INTO REST_DB_ACCESS("ID", "NAME", "PASSWORD", "ROLE") VALUES (4, 'Leonard', 'password', 'guest')
+INSERT INTO REST_DB_ACCESS("ID", "NAME", "PASSWORD", "ROLE") VALUES (5, 'Bernadette', 'password', 'guest')
+INSERT INTO REST_DB_ACCESS("ID", "NAME", "PASSWORD", "ROLE") VALUES (6, 'Raj', 'password', 'guest')
+INSERT INTO REST_DB_ACCESS("ID", "NAME", "PASSWORD", "ROLE") VALUES (7, 'Howard', 'password', 'guest')
+INSERT INTO REST_DB_ACCESS("ID", "NAME", "PASSWORD", "ROLE") VALUES (8, 'Priya', 'password', 'guest')
